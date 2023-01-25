0a8f8bc2b45e88b57697f3fdfc8ad696950751cc
Pingvu <nhlpool27@gmail.com>
2023-01-25 11:53:23 +0100

    Add project files.

---
+++ uppgift 5.7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] länder = { "Kina", "indien", "USA", "Indonesien", "Pakistan" };
+
+            for(int i = 0; i<länder.Length; i++)
+            {
+                Console.WriteLine(länder[i]);
+            }
+            Console.WriteLine();
+            Console.Write("Hur många länder vill du skriva utt: ");
+            string val = Console.ReadLine();
+
+            int val2 = int.Parse(val);
+
+            Console.WriteLine();
+            for(int i = 0; i < val2; i++)
+            {
+                Console.WriteLine(länder[i]);
+            }
+        }
+    }
+}
